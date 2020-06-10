f9b481d096e520ec31800a26ab26d3187902f654
Léo SENIQUE <leo.senique@gmail.com>
2020-06-10 15:48:37 +0200

    ajout TP4

---
+++ src/main/java/utils/ConversionNombre.java
@@ -0,0 +1,14 @@
+package utils;
+
+public class ConversionNombre {
+
+	public static void main(String[] args) {
+		String s = "18";
+		int s2 = Integer.parseInt(s);
+		
+		int a=1;
+		int b=2;
+		System.out.println(Integer.sum(a, b));
+	}
+
+}
