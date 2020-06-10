aade976148b6b8f15ea9971c192f2934720b0712
Léo SENIQUE <leo.senique@gmail.com>
2020-06-10 15:52:51 +0200

    ajout TP5

---
+++ src/main/java/fr/diginamic/banque/entites/Compte.java
@@ -0,0 +1,13 @@
+package fr.diginamic.banque.entites;
+
+public class Compte {
+	private int num;
+	private int solde;
+	
+	public Compte(int num, int solde) {
+		this.num = num;
+		this.solde = solde;
+	}
+	
+
+}
---
+++ src/main/java/fr/diginamic/banque/entites/TestBanque.java
@@ -0,0 +1,11 @@
+package fr.diginamic.banque.entites;
+
+public class TestBanque {
+
+	public static void main(String[] args) {
+		Compte c1 = new Compte(01,500);
+		System.out.println(c1);
+
+	}
+
+}
