100d7921265bbe20a97575f4179889e927809591
Léo SENIQUE <leo.senique@gmail.com>
2020-06-10 17:03:12 +0200

    Ajout TP8

---
+++ src/main/java/fr/diginamic/banque/entites/Operation.java
@@ -0,0 +1,30 @@
+package fr.diginamic.banque.entites;
+
+public abstract class Operation {
+	private String date;
+	private float montant;
+	public Operation(String date, float montant) {
+		this.date = date;
+		this.montant = montant;
+	}
+	
+	abstract String afficherType();
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public float getMontant() {
+		return montant;
+	}
+
+	public void setMontant(float montant) {
+		this.montant = montant;
+	}
+	
+
+}
---
+++ src/main/java/fr/diginamic/banque/entites/TestOperation.java
@@ -0,0 +1,34 @@
+package fr.diginamic.banque.entites;
+
+public class TestOperation {
+
+	public static void main(String[] args) {
+		
+		Credit op1 = new Credit("10/06/2020",80502.00F);
+		Credit op2 = new Credit("09/06/2020",5.00F);
+		Debit op3 = new Debit("01/01/1999",20.50F);
+		Debit op4 = new Debit("25/05/2019",65456.12F);
+		
+		float res = 0F;
+		
+		Operation[] tabOp = {op1,op2,op3,op4};
+		for(int i=0 ; i < tabOp.length ; i++) {
+			String type = tabOp[i].afficherType();
+			float montant = tabOp[i].getMontant();
+			System.out.println("Informations du compte "+(i+1));
+			System.out.println("date : "+tabOp[i].getDate());
+			System.out.println("montant : "+montant+"€");
+
+			System.out.println("type : "+type);
+			if(type=="Crédit") {
+				res += montant;
+			}
+			else {
+				res -= montant;
+			}
+		}
+		
+		System.out.println("Montant global : "+res+"€");
+	}
+
+}
