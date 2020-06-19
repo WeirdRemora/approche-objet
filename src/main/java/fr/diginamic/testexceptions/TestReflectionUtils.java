0d58674993b0feff05ed48945d0cea0fe8d44df7
Léo SENIQUE <leo.senique@gmail.com>
2020-06-19 16:22:00 +0200

    remplacement des catch par un multicatch

--- src/main/java/fr/diginamic/testexceptions/TestReflectionUtils.java
+++ src/main/java/fr/diginamic/testexceptions/TestReflectionUtils.java
@@ -7,15 +7,12 @@ public class TestReflectionUtils {
 	public static void main(String[] args) {
 		//Pays pays = new Pays("Léopotamie",5000,2000);
 		Pays pays = null;
-		try {
-			ReflectionUtils.afficherAttributs(pays);
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (ReflectionException e) {
-			System.out.println(e.getMessage());;
-		}
+			try {
+				ReflectionUtils.afficherAttributs(pays);
+			} catch (IllegalArgumentException | IllegalAccessException | ReflectionException e) {
+				System.out.println(e.getMessage());
+			}
+
 
 	}
 
